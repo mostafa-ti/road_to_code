f38dc8390c1dddb5b25f1c8289820a073226d857
Mostafa <mostafa.torbati@gmail.com>
2026-03-09 11:18:40 +0100

    prices printed correctly

--- pizza_order/main.py
+++ pizza_order/main.py
@@ -36,3 +36,11 @@ print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
+bill = 0
+if size.lower() == "l":
+    bill = 25
+elif size.lower() == "m":
+    bill = 20
+else:
+    bill = 15
+print(bill)
